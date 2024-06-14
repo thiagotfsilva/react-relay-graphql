95b06badeaab8841ee652c61957ba13d4a36aaeb
thiagotfsilva <thiagophsilva@hotmail.com>
2024-06-13 22:50:51 -0300

    create transactionsCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { TransactionCard } from "./components/TransactionCard";
 
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
-  )
+    <div className="container h-screen pt-4 px-3">
+      <div className="grid grid-cols-12 h-full text-center">
+        <div className="col-span-3">menu</div>
+        <div className="col-span-6">main</div>
+        <div className="col-span-3 border-2 rounded-sm p-1">
+          <h3>Transações</h3>
+          <div>
+            <TransactionCard />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
